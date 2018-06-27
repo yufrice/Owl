18dc20ad614c9d4e6f7513b2101194ace911bb29
yufrice <c155208@cstu.nit.ac.jp>
2018-06-27 14:54:29 +0900

    feach prams dynamic

--- docker/webserver/pull.sh
+++ docker/webserver/pull.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 repo='Aripiprazole%2FOwl'
-url="https://gitlab.com/api/v4/projects/${repo}/jobs/artifacts/v0.0.1/download?job=deploy"
+url="https://gitlab.com/api/v4/projects/${repo}/jobs/artifacts/${ReleaseVer}/download?job=deploy"
 header="Private-Token: ${GitLab_Token}"
 
 eval "curl -L -o artifacts.zip -H "'"$header"'" $url"
