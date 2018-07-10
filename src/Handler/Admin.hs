baab1f384f42fdb5623c6a00429f8b83b03d7777
yufrice <c155208@cstu.nit.ac.jp>
2018-07-10 22:12:26 +0900

    Fix format

--- src/Handler/Admin.hs
+++ src/Handler/Admin.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
 module Handler.Admin where
 
 import Import
+import qualified Yesod.Form.Bootstrap4 as Bs4
+
+data FileForm = FileForm
+    { fileName :: Text
+    , fileVec :: Text
+    , fileImage :: FileInfo 
+    }
 
 getAdminR :: Handler Html
-getAdminR = error "Not yet implemented: getAdminR"
+getAdminR = do
+    products <- runDB $ selectList [] [Asc ProductId]
+    (formWidget, enctype) <- generateFormPost fileForm
+    defaultLayout $(widgetFile "admin")
+
+fileForm :: Form FileForm
+fileForm = Bs4.renderBootstrap4 Bs4.BootstrapBasicForm $ FileForm
+    <$> areq textField (Bs4.bfs (""::Text)) Nothing
+    <*> areq textField (Bs4.bfs (""::Text)) Nothing
+    <*> fileAFormReq ""
+    <* Bs4.bootstrapSubmit ("Submit" :: Bs4.BootstrapSubmit Text)
--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -9,19 +9,6 @@ module Handler.Home where
 import Import
 import qualified Yesod.Form.Bootstrap4 as Bs4
 
--- Define our data that will be used for creating the form.
-data FileForm = FileForm
-    { fileInfo :: FileInfo
-    , fileDescription :: Text
-    }
-
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
     (searchWidget, enctype) <- generateFormPost searchForm
@@ -31,9 +18,8 @@ getHomeR = do
             <div .container-fluid>
                 <div .row>
                     ^{navber searchWidget enctype}
-                    <div .col-md-8>
-                        <div .container>
-                            ^{display}
+                    <div .col-lg-8>
+                        ^{display}
         |]
 
 postHomeR :: Handler Html
@@ -48,19 +34,26 @@ postHomeR = do
                     <div .container-fluid>
                         <div .row>
                             ^{navber searchWidget enctype}
-                            <div .col-md-8>
-                                <div .container>
-                                    ^{display}
-                                    ^{resultView products}
+                            <div .col-lg-8>
+                                ^{display}
+                                ^{resultView products}
                 |]
-        _-> defaultLayout $(widgetFile "homepage")
+        _-> defaultLayout $ do
+            setTitle "home"
+            [whamlet|
+                <div .container-fluid>
+                    <div .row>
+                        ^{navber searchWidget enctype}
+                        <div .col-lg-8>
+                            ^{display}
+            |]
 
 navber :: Widget -> Enctype -> Widget
 navber searchWidget enctype = $(widgetFile "navber")
 
 display :: Widget
 display = toWidget [whamlet|
-        <div .jumbotron>
+        <div .jumbotron.mt-5>
             <h1 .display-4>
                 text
             <p>
@@ -70,7 +63,7 @@ display = toWidget [whamlet|
 resultView :: [Entity Product] -> Widget
 resultView products = $(widgetFile "result")
 
-searchForm:: Form Text
+searchForm :: Form Text
 searchForm = Bs4.renderBootstrap4 Bs4.BootstrapBasicForm
         $ areq textField (Bs4.bfs (""::Text)) Nothing
         <* Bs4.bootstrapSubmit ("Search" :: Bs4.BootstrapSubmit Text)
